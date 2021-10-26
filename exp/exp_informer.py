b66d05fdd4d8cca483e4269dfeb8bb9736474f53
kiranmadhusud <kiranmadhusud@ismll.de>
2021-10-26 20:04:39 +0200

    first commit

---
+++ exp/exp_informer.py
@@ -0,0 +1,365 @@
+from data.data_loader import Dataset_ETT_hour, Dataset_ETT_minute, Dataset_Custom, Dataset_Pred, Dataset_ECL_hour
+from exp.exp_basic import Exp_Basic
+from models.model import Informer, Yformer
+
+from utils.tools import EarlyStopping, adjust_learning_rate
+from utils.metrics import metric
+
+import numpy as np
+
+import torch
+import torch.nn as nn
+from torch import optim
+from torch.utils.data import DataLoader
+from torchinfo import summary
+
+import os
+import time
+
+import warnings
+warnings.filterwarnings('ignore')
+
+class Exp_Informer(Exp_Basic):
+    def __init__(self, args):
+        super(Exp_Informer, self).__init__(args)
+    
+    def _build_model(self):
+        model_dict = {
+            'informer':Informer,
+            'yformer':Yformer,
+
+        }
+        if self.args.model=='informer'or self.args.model=="yformer":
+            model = model_dict[self.args.model](
+                self.args.enc_in,
+                self.args.dec_in, 
+                self.args.c_out, 
+                self.args.seq_len, 
+                self.args.label_len,
+                self.args.pred_len, 
+                self.args.factor,
+                self.args.d_model, 
+                self.args.n_heads, 
+                self.args.e_layers,
+                self.args.d_layers, 
+                self.args.d_ff,
+                self.args.dropout, 
+                self.args.attn,
+                self.args.embed,
+                self.args.freq,
+                self.args.activation,
+                self.args.output_attention,
+                self.args.distil,
+                self.device
+            ).float()
+        
+        if self.args.use_multi_gpu and self.args.use_gpu:
+            model = nn.DataParallel(model, device_ids=self.args.device_ids)
+        return model
+
+    def _get_data(self, flag):
+        args = self.args
+
+        data_dict = {
+            'ETTh1':Dataset_ETT_hour,
+            'ETTh2':Dataset_ETT_hour,
+            'ETTm1':Dataset_ETT_minute,
+            'ETTm2':Dataset_ETT_minute,
+            'ECL':Dataset_ECL_hour,
+            'custom':Dataset_Custom,
+        }
+        Data = data_dict[self.args.data]
+        timeenc = 0 if args.embed!='timeF' else 1
+
+        if flag == 'test':
+            shuffle_flag = False; drop_last = True; batch_size = args.batch_size; freq=args.freq
+        elif flag=='pred':
+            shuffle_flag = False; drop_last = False; batch_size = 1; freq=args.detail_freq
+            Data = Dataset_Pred
+        else:
+            shuffle_flag = True; drop_last = True; batch_size = args.batch_size; freq=args.freq
+        
+        data_set = Data(
+            root_path=args.root_path,
+            data_path=args.data_path,
+            flag=flag,
+            size=[args.seq_len, args.label_len, args.pred_len],
+            features=args.features,
+            target=args.target,
+            use_decoder_tokens=args.use_decoder_tokens,
+            timeenc=timeenc,
+            freq=freq
+        )
+        print(flag, len(data_set))
+        data_loader = DataLoader(
+            data_set,
+            batch_size=batch_size,
+            shuffle=shuffle_flag,
+            num_workers=args.num_workers,
+            drop_last=drop_last)
+
+        return data_set, data_loader
+
+    def _select_optimizer(self):
+        model_optim = optim.Adam(self.model.parameters(), lr=self.args.learning_rate, weight_decay=self.args.weight_decay)
+        return model_optim
+    
+    def _select_criterion(self):
+        criterion =  nn.MSELoss()
+        return criterion
+
+    def vali(self, vali_data, vali_loader, criterion):
+        self.model.eval()
+        total_loss = []
+        for i, (batch_x,batch_y,batch_x_mark,batch_y_mark) in enumerate(vali_loader):
+            batch_x = batch_x.float().to(self.device)
+            batch_y = batch_y.float()
+            
+            batch_x_mark = batch_x_mark.float().to(self.device)
+            batch_y_mark = batch_y_mark.float().to(self.device)
+
+            # decoder input
+            dec_inp = torch.zeros_like(batch_y[:,-self.args.pred_len:,:]).float()
+
+            if self.args.use_decoder_tokens:
+                dec_inp = torch.cat([batch_y[:,:self.args.label_len,:], dec_inp], dim=1).float().to(self.device)
+            else:
+                dec_inp = dec_inp.float().to(self.device)
+
+            # encoder - decoder
+            if self.args.use_amp:
+                with torch.cuda.amp.autocast():
+                    if self.args.output_attention:
+                        outputs = self.model(batch_x, batch_x_mark, dec_inp, batch_y_mark)[0]
+                    else:
+                        outputs = self.model(batch_x, batch_x_mark, dec_inp, batch_y_mark)[:, -self.args.pred_len:, :]
+            else:
+                if self.args.output_attention:
+                    outputs = self.model(batch_x, batch_x_mark, dec_inp, batch_y_mark)[0]
+                else:
+                    outputs = self.model(batch_x, batch_x_mark, dec_inp, batch_y_mark)[:, -self.args.pred_len:, :]
+            f_dim = -1 if self.args.features=='MS' else 0
+            batch_y = batch_y[:,-self.args.pred_len:,f_dim:].to(self.device)
+
+            pred = outputs.detach().cpu()
+            true = batch_y.detach().cpu()
+
+            loss = criterion(pred, true) 
+
+            total_loss.append(loss)
+        total_loss = np.average(total_loss)
+        self.model.train()
+        return total_loss
+        
+    def train(self, setting):
+        train_data, train_loader = self._get_data(flag = 'train')
+        vali_data, vali_loader = self._get_data(flag = 'val')
+        test_data, test_loader = self._get_data(flag = 'test')
+
+        path = os.path.join(self.args.checkpoints, setting)
+        if not os.path.exists(path):
+            os.makedirs(path)
+
+        time_now = time.time()
+        
+        train_steps = len(train_loader)
+        early_stopping = EarlyStopping(patience=self.args.patience, verbose=True)
+        
+        model_optim = self._select_optimizer()
+        criterion =  self._select_criterion()
+
+        if self.args.use_amp:
+            scaler = torch.cuda.amp.GradScaler()
+
+        for i, (batch_x,batch_y,batch_x_mark,batch_y_mark) in enumerate(train_loader):
+            summary(self.model,  [batch_x.shape, batch_x_mark.shape, batch_y.shape, batch_y_mark.shape]) # show the size 
+            break
+
+        for epoch in range(self.args.train_epochs):
+            iter_count = 0
+            train_loss = []
+            auto_train_loss = []
+            combined_train_loss = []
+            self.model.train()
+            epoch_time = time.time()
+            for i, (batch_x,batch_y,batch_x_mark,batch_y_mark) in enumerate(train_loader):
+                iter_count += 1
+                
+                model_optim.zero_grad()
+                
+                batch_x = batch_x.float().to(self.device)
+                batch_y = batch_y.float()
+                
+                batch_x_mark = batch_x_mark.float().to(self.device)
+                batch_y_mark = batch_y_mark.float().to(self.device)
+
+                # decoder input
+                dec_inp = torch.zeros_like(batch_y[:,-self.args.pred_len:,:]).float()
+                
+                if self.args.use_decoder_tokens:
+                    dec_inp = torch.cat([batch_y[:,:self.args.label_len,:], dec_inp], dim=1).float().to(self.device)
+                else:
+                    dec_inp = dec_inp.float().to(self.device)
+                
+                if self.args.output_attention:
+                    outputs = self.model(batch_x, batch_x_mark, dec_inp, batch_y_mark)[0]
+                else:
+                    outputs = self.model(batch_x, batch_x_mark, dec_inp, batch_y_mark)
+
+                f_dim = -1 if self.args.features=='MS' else 0
+                batch_y = batch_y[:,-self.args.pred_len:,f_dim:].to(self.device)
+                auto_loss = criterion(outputs[:, :-self.args.pred_len,:], batch_x)
+                auto_train_loss.append(auto_loss.item())
+                loss = criterion(outputs[:, -self.args.pred_len:,:], batch_y)
+                train_loss.append(loss.item())
+
+                combined_loss = self.args.alpha * auto_loss + (1-self.args.alpha) * loss
+                combined_train_loss.append(combined_loss.item())
+                
+                if (i+1) % 100==0:
+                    print("\titers: {0}, epoch: {1} | loss: {2:.7f} | auto loss: {3:.7f} | comb loss: {4:.7f}".format(i + 1, epoch + 1, loss.item(), auto_loss.item(), combined_loss.item()))
+                    speed = (time.time()-time_now)/iter_count
+                    left_time = speed*((self.args.train_epochs - epoch)*train_steps - i)
+                    print('\tspeed: {:.4f}s/iter; left time: {:.4f}s'.format(speed, left_time))
+                    iter_count = 0
+                    time_now = time.time()
+                
+                combined_loss.backward()
+                model_optim.step()
+            print("Epoch: {} cost time: {}".format(epoch+1, time.time()-epoch_time))
+            train_loss = np.average(train_loss)
+            auto_loss = np.average(auto_train_loss)
+            combined_loss = np.average(combined_train_loss)
+            vali_loss = self.vali(vali_data, vali_loader, criterion)
+            test_loss = self.vali(test_data, test_loader, criterion)
+            
+            print("Epoch: {0}, Steps: {1} | Train Loss: {2:.7f} | Auto Loss : {3:.7f} | Comb Loss : {4:.7f}, Vali Loss: {5:.7f} Test Loss: {6:.7f}".format(
+                epoch + 1, train_steps, train_loss, auto_loss, combined_loss, vali_loss, test_loss))
+            early_stopping(vali_loss, self.model, path)
+            if early_stopping.early_stop:
+                print("Early stopping")
+                break
+
+            adjust_learning_rate(model_optim, epoch+1, self.args)
+            
+        best_model_path = path+'/'+'checkpoint.pth'
+        self.model.load_state_dict(torch.load(best_model_path))
+        
+        return self.model
+
+    def test(self, setting):
+        test_data, test_loader = self._get_data(flag='test')
+        
+        self.model.eval()
+        
+        preds = []
+        trues = []
+        
+        for i, (batch_x,batch_y,batch_x_mark,batch_y_mark) in enumerate(test_loader):
+            batch_x = batch_x.float().to(self.device)
+            batch_y = batch_y.float()
+            batch_x_mark = batch_x_mark.float().to(self.device)
+            batch_y_mark = batch_y_mark.float().to(self.device)
+
+            # decoder input
+            dec_inp = torch.zeros_like(batch_y[:,-self.args.pred_len:,:]).float()
+            if self.args.use_decoder_tokens:
+                dec_inp = torch.cat([batch_y[:,:self.args.label_len,:], dec_inp], dim=1).float().to(self.device)
+            else:
+                dec_inp = dec_inp.float().to(self.device)
+            # encoder - decoder
+            if self.args.use_amp:
+                with torch.cuda.amp.autocast():
+                    if self.args.output_attention:
+                        outputs = self.model(batch_x, batch_x_mark, dec_inp, batch_y_mark)[0]
+                    else:
+                        outputs = self.model(batch_x, batch_x_mark, dec_inp, batch_y_mark)[:, -self.args.pred_len:, :]
+            else:
+                if self.args.output_attention:
+                    outputs = self.model(batch_x, batch_x_mark, dec_inp, batch_y_mark)[0]
+                else:
+                    outputs = self.model(batch_x, batch_x_mark, dec_inp, batch_y_mark)[:, -self.args.pred_len:, :]
+            f_dim = -1 if self.args.features=='MS' else 0
+            batch_y = batch_y[:,-self.args.pred_len:,f_dim:].to(self.device)
+            
+            pred = outputs.detach().cpu().numpy()#.squeeze()
+            true = batch_y.detach().cpu().numpy()#.squeeze()
+
+            preds.append(pred)
+            trues.append(true)
+
+        preds = np.array(preds)
+        trues = np.array(trues)
+        print('test shape:', preds.shape, trues.shape)
+        preds = preds.reshape(-1, preds.shape[-2], preds.shape[-1])
+        trues = trues.reshape(-1, trues.shape[-2], trues.shape[-1])
+        print('test shape:', preds.shape, trues.shape)
+
+        # result save
+        folder_path = './results/' + setting +'/'
+        if not os.path.exists(folder_path):
+            os.makedirs(folder_path)
+
+        mae, mse, rmse, mape, mspe = metric(preds, trues)
+        print('mse:{}, mae:{}'.format(mse, mae))
+
+        np.save(folder_path+'metrics.npy', np.array([mae, mse, rmse, mape, mspe]))
+        np.save(folder_path+'pred.npy', preds)
+        np.save(folder_path+'true.npy', trues)
+
+        return
+
+    def predict(self, setting, load=False):
+        pred_data, pred_loader = self._get_data(flag='pred')
+        
+        if load:
+            path = os.path.join(self.args.checkpoints, setting)
+            best_model_path = path+'/'+'checkpoint.pth'
+            self.model.load_state_dict(torch.load(best_model_path))
+
+        self.model.eval()
+        
+        preds = []
+        
+        for i, (batch_x,batch_y,batch_x_mark,batch_y_mark) in enumerate(pred_loader):
+            batch_x = batch_x.float().to(self.device)
+            batch_y = batch_y.float()
+            batch_x_mark = batch_x_mark.float().to(self.device)
+            batch_y_mark = batch_y_mark.float().to(self.device)
+
+            # decoder input
+            dec_inp = torch.zeros_like(batch_y[:,-self.args.pred_len:,:]).float()
+            if self.args.use_decoder_tokens:
+                dec_inp = torch.cat([batch_y[:,:self.args.label_len,:], dec_inp], dim=1).float().to(self.device)
+            else:
+                dec_inp = dec_inp.float().to(self.device)
+            # encoder - decoder
+            if self.args.use_amp:
+                with torch.cuda.amp.autocast():
+                    if self.args.output_attention:
+                        outputs = self.model(batch_x, batch_x_mark, dec_inp, batch_y_mark)[0]
+                    else:
+                        outputs = self.model(batch_x, batch_x_mark, dec_inp, batch_y_mark)
+            else:
+                if self.args.output_attention:
+                    outputs = self.model(batch_x, batch_x_mark, dec_inp, batch_y_mark)[0]
+                else:
+                    outputs = self.model(batch_x, batch_x_mark, dec_inp, batch_y_mark)
+            f_dim = -1 if self.args.features=='MS' else 0
+            batch_y = batch_y[:,-self.args.pred_len:,f_dim:].to(self.device)
+            
+            pred = outputs.detach().cpu().numpy()#.squeeze()
+            
+            preds.append(pred)
+
+        preds = np.array(preds)
+        preds = preds.reshape(-1, preds.shape[-2], preds.shape[-1])
+        
+        # result save
+        folder_path = './results/' + setting +'/'
+        if not os.path.exists(folder_path):
+            os.makedirs(folder_path)
+        
+        np.save(folder_path+'real_prediction.npy', preds)
+        
+        return
---
+++ models/decoder.py
@@ -0,0 +1,162 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from models.attn import FullAttention, ProbAttention, AttentionLayer
+
+debug=False
+
+class ConvLayer(nn.Module):
+    def __init__(self, c_in):
+        super(ConvLayer, self).__init__()
+        self.downConv = nn.Conv1d(in_channels=c_in,
+                                  out_channels=c_in,
+                                  kernel_size=3,
+                                  padding=2,
+                                  padding_mode='circular')
+        self.norm = nn.BatchNorm1d(c_in)
+        self.activation = nn.ELU()
+        self.maxPool = nn.MaxPool1d(kernel_size=3, stride=2, padding=1)
+
+    def forward(self, x):
+        if debug:
+            print("input x ", x.shape)
+
+        x = self.downConv(x.permute(0, 2, 1))
+        x = self.norm(x)
+        x = self.activation(x)
+        x = self.maxPool(x)
+        x = x.transpose(1,2)
+        if debug:
+            print("down conv", x.shape)
+
+        return x
+
+class DeConvLayer(nn.Module):
+    def __init__(self, c_in, c_out =None):
+        super(DeConvLayer, self).__init__()
+        c_out = c_in if c_out is None else c_out
+        self.upConv = nn.ConvTranspose1d(in_channels=c_in,
+                                  out_channels=c_out,
+                                  kernel_size=3,
+                                  stride=2,
+                                  padding=2)
+        self.norm = nn.BatchNorm1d(c_out)
+        self.activation = nn.ELU()
+        # self.maxPool = nn.MaxPool1d(kernel_size=3, stride=2, padding=1)
+
+    def forward(self, x):
+        x = self.upConv(x.permute(0, 2, 1))
+        x = self.norm(x)
+        x = self.activation(x)
+        # x = self.maxPool(x)
+        x = x.transpose(1,2)
+        return x
+
+
+class DecoderLayer(nn.Module):
+    def __init__(self, self_attention, cross_attention, d_model, d_ff=None,
+                 dropout=0.1, activation="relu"):
+        super(DecoderLayer, self).__init__()
+        d_ff = d_ff or 4*d_model
+        self.self_attention = self_attention
+        self.cross_attention = cross_attention
+        self.conv1 = nn.Conv1d(in_channels=d_model, out_channels=d_ff, kernel_size=1)
+        self.conv2 = nn.Conv1d(in_channels=d_ff, out_channels=d_model, kernel_size=1)
+        self.norm1 = nn.LayerNorm(d_model)
+        self.norm2 = nn.LayerNorm(d_model)
+        self.norm3 = nn.LayerNorm(d_model)
+        self.dropout = nn.Dropout(dropout)
+        self.activation = F.relu if activation == "relu" else F.gelu
+
+    def forward(self, x, cross, x_mask=None, cross_mask=None):
+
+        x = x + self.dropout(self.cross_attention(
+            x, cross, cross,
+            attn_mask=cross_mask
+        )[0])
+        y = x = self.norm2(x)
+        y = self.dropout(self.activation(self.conv1(y.transpose(-1,1))))
+        y = self.dropout(self.conv2(y).transpose(-1,1))
+
+        return self.norm3(x+y)
+
+class Decoder(nn.Module):
+    def __init__(self, layers, norm_layer=None):
+        super(Decoder, self).__init__()
+        self.layers = nn.ModuleList(layers)
+        self.norm = norm_layer
+
+    def forward(self, x, cross, x_mask=None, cross_mask=None):
+        for layer in self.layers:
+            x = layer(x, cross, x_mask=x_mask, cross_mask=cross_mask)
+
+        if self.norm is not None:
+            x = self.norm(x)
+
+        return x
+
+
+class YformerDecoderLayer(nn.Module):
+    def __init__(self,  cross_attention, d_model, d_ff=None, dropout=0.1, activation="relu"):
+        super(YformerDecoderLayer, self).__init__()
+        d_ff = d_ff or 4*d_model
+        # self.self_attention = self_attention
+        self.cross_attention = cross_attention
+        self.conv1 = nn.Conv1d(in_channels=d_model, out_channels=d_ff, kernel_size=1)
+        self.conv2 = nn.Conv1d(in_channels=d_ff, out_channels=d_model, kernel_size=1)
+        self.norm1 = nn.LayerNorm(d_model)
+        self.norm2 = nn.LayerNorm(d_model)
+        self.norm3 = nn.LayerNorm(d_model)
+        self.dropout = nn.Dropout(dropout)
+        self.activation = F.relu if activation == "relu" else F.gelu
+
+    def forward(self, x, cross, cross_mask =None):
+
+        x = x + self.dropout(self.cross_attention(
+            x, cross, cross,
+            attn_mask=cross_mask
+        )[0])
+
+        x = x + self.dropout(x)
+
+        y = x = self.norm2(x)
+        y = self.dropout(self.activation(self.conv1(y.transpose(-1,1))))
+        y = self.dropout(self.conv2(y).transpose(-1,1))
+
+        return self.norm3(x+y)
+
+
+
+class YformerDecoder(nn.Module):
+    def __init__(self, attn_layers=None, conv_layers=None, norm_layer=None):
+        super(YformerDecoder, self).__init__()
+        self.attn_layers = nn.ModuleList(attn_layers) if attn_layers is not None else None
+        self.conv_layers = nn.ModuleList(conv_layers) if conv_layers is not None else None
+        self.first_conv = DeConvLayer(c_in=512)
+        self.first_attn = YformerDecoderLayer(AttentionLayer(FullAttention(False), d_model=512, n_heads=8), d_model =512, d_ff = 2048)
+        self.norm = norm_layer
+
+    def forward(self, x_list, fut_x_list, attn_mask=None):
+        # x [B, L, D]
+        attns = []
+        x = x_list.pop(0)
+        fut_x = fut_x_list.pop(0)
+        x = self.first_attn(x, fut_x, cross_mask=attn_mask)
+        x = self.first_conv(x) # upsample to connect with other layers from encoder
+        if self.conv_layers is not None:
+            if self.attn_layers is not None:
+                for cross_x, cross_fut_x, attn_layer, conv_layer in zip(x_list, fut_x_list, self.attn_layers, self.conv_layers):
+                    cross = torch.cat((cross_x,cross_fut_x), dim=1)
+                    x = attn_layer(x, cross, cross_mask=attn_mask)
+                    x = conv_layer(x)
+            else:
+                # pipeline for only convolution layers
+                for conv_layer in self.conv_layers:
+                    x = conv_layer(x)
+        else:
+            for attn_layer in self.attn_layers:
+                x, attn = attn_layer(x, attn_mask=attn_mask)
+
+        if self.norm is not None: 
+            x = self.norm(x)
+        return x, attns
